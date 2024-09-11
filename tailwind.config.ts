5dfa04adaf570b5751eaac86e61199feee334ce9
tolly <40471358+orrefailaT@users.noreply.github.com>
2024-09-10 23:12:45 -0400

    tailwind styles

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "tokyo-night": {
+          bg: "#1a1b26",
+          fg: "#a9b1d6",
+          accent1: "#bb9af7",
+          accent2: "#7aa2f7",
+          accent3: "#7dcfff",
+        },
+      },
+      fontFamily: {
+        mono: ["JetBrains Mono", "monospace"],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ["hover"],
+      transform: ["hover"],
+    },
+  },
+  plugins: ["@tailwindcss/typography"],
+};
